179b9a244e9a7bcc7b9ec3727d8eba13bd39171e
matevip <7333791@qq.com>
2020-11-03 22:26:56 +0800

    修改在线文档的地址

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="dashboard-container" id="dashboard">
-    <div class="dashboard-text">欢迎光临: {{name}} </div>
+    <div class="dashboard-text">欢迎光临: {{ name }}</div>
 
     <el-row :gutter="20">
       <el-col :span="6">
         <el-card class="box-card">
           <el-col :span="12">
-            <svg-icon icon-class="dou" ></svg-icon>
+            <svg-icon icon-class="dou"></svg-icon>
           </el-col>
-          <el-col :span="12" >
+          <el-col :span="12">
             <div class="card-panel-text">在线文档</div>
             <div class="card-panel-num">
-              <a href="http://doc.mate.vip" target="_blank">Document</a>
+              <a href="https://mate.vip/#/docs" target="_blank">Document</a>
             </div>
           </el-col>
         </el-card>
@@ -19,9 +19,9 @@
       <el-col :span="6">
         <el-card class="box-card">
           <el-col :span="12">
-            <svg-icon  icon-class="view" ></svg-icon>
+            <svg-icon icon-class="view"></svg-icon>
           </el-col>
-          <el-col :span="12" >
+          <el-col :span="12">
             <div class="card-panel-text">新增用户</div>
             <div class="card-panel-num">1024</div>
           </el-col>
@@ -30,9 +30,9 @@
       <el-col :span="6">
         <el-card class="box-card">
           <el-col :span="12">
-            <svg-icon  icon-class="message" ></svg-icon>
+            <svg-icon icon-class="message"></svg-icon>
           </el-col>
-          <el-col :span="12" >
+          <el-col :span="12">
             <div class="card-panel-text">未处理消息</div>
             <div class="card-panel-num">100</div>
           </el-col>
@@ -41,9 +41,9 @@
       <el-col :span="6">
         <el-card class="box-card">
           <el-col :span="12">
-            <svg-icon  icon-class="mall" ></svg-icon>
+            <svg-icon icon-class="mall"></svg-icon>
           </el-col>
-          <el-col :span="12" >
+          <el-col :span="12">
             <div class="card-panel-text">营业收入</div>
             <div class="card-panel-num">100,000</div>
           </el-col>
@@ -54,63 +54,61 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  export default {
-    name: 'Dashboard',
-    name: '首页',
-    computed: {
-      ...mapGetters([
-        'name'
-      ])
-    }
+import { mapGetters } from "vuex";
+export default {
+  name: "Dashboard",
+  name: "首页",
+  computed: {
+    ...mapGetters(["name"])
   }
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  .el-row{
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
   }
+}
 
-  .dashboard {
-    &-container {
-      padding: 15px;
-      background-color: #f0f2f5;
-    }
-    &-text {
-      font-size: 14px;
-      line-height: 30px;
-      padding-bottom:15px;
-    }
+.dashboard {
+  &-container {
+    padding: 15px;
+    background-color: #f0f2f5;
   }
-  .box-card{
-    height:108px;
+  &-text {
+    font-size: 14px;
+    line-height: 30px;
+    padding-bottom: 15px;
   }
-  .chart{
-    height: 350px;
-  }
-  .section{
-    padding:20px;
-    background-color: white;
-    border: 1px solid #ebeef5;
-    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
-  }
+}
+.box-card {
+  height: 108px;
+}
+.chart {
+  height: 350px;
+}
+.section {
+  padding: 20px;
+  background-color: white;
+  border: 1px solid #ebeef5;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
 
-  .box-card > div >div > .svg-icon {
-    width: 4em;
-    height: 4em;
-    color:#34bfa3;
-  }
-  .card-panel-text{
-    padding-top:10px;
-    font-size:16px;
-    color:gray;
-  }
-  .card-panel-num{
-    padding-top:10px;
-    font-size:20px;
-    font-weight: bold;
-  }
+.box-card > div > div > .svg-icon {
+  width: 4em;
+  height: 4em;
+  color: #34bfa3;
+}
+.card-panel-text {
+  padding-top: 10px;
+  font-size: 16px;
+  color: gray;
+}
+.card-panel-num {
+  padding-top: 10px;
+  font-size: 20px;
+  font-weight: bold;
+}
 </style>
