fc52c836238f72d234c565065aedbf35c2467422
matevip <7333791@qq.com>
2021-08-30 11:17:49 +0800

    refactor🎨:  新增微服务管理功能

---
+++ src/api/gateway/route.ts
@@ -0,0 +1,18 @@
+import { RouteVO, RouteDTO, Route } from './model/routeModel';
+import { defHttp } from '/@/utils/http/axios';
+
+enum Api {
+  Page = '/mate-system/route/page',
+  Set = '/mate-system/route/set',
+  Del = '/mate-system/route/del',
+}
+
+// 分页查询
+export const page = (params: RouteVO) => defHttp.get<RouteDTO>({ url: Api.Page, params });
+
+// 保存
+export const set = (params: Route) => defHttp.post<Route>({ url: Api.Set, params });
+
+// 删除
+export const del = (params: { ids: String }) =>
+  defHttp.post<boolean>({ url: Api.Del + `?ids=${params.ids}` });
